7eaaa60040b6305ca3d31819fa63cf72f887b994
mdarafathassan8-code <mdarafathassan8@gmail.com>
2026-08-22 08:58:22 +0600

    Add Android GeckoView project settings

---
+++ android/settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral(); maven { url = uri("https://maven.mozilla.org/maven2/") } } }
+rootProject.name = "NovaMobile"
+include(":app")
